ff9ed2e84155a1d58f600bbbc2369c75c225e3a8
Jean Paul Baladi <yanbaladi159@gmail.com>
2021-10-12 16:03:04 -0300

    Cart-View

--- src/Components/navBar/navBar.js
+++ src/Components/navBar/navBar.js
@@ -9,7 +9,6 @@ const NavBar = () => {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/1">Comida</Link></li>
                     <li><Link to="/2">Articulos</Link></li>
-                    <li><Link to="/Cart">Carrito</Link></li>
                     <li><CarWidget/></li>
                 </ul>
             </div>
